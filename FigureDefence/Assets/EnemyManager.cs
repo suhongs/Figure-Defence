4ab6b6cf9279eb472644600e02bb548bb8dd59b8
suhongs <rlatnghd1236@gmail.com>
2020-11-12 01:41:40 +0900

    MapUI
    Map & Enemy Create

---
+++ FigureDefence/Assets/EnemyManager.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+
+public class EnemyManager : MonoBehaviour
+{
+    public float speed = 10.0f;
+
+    private Transform target;
+    private int wavePointIndex = 0;
+
+    private void Start()
+    {
+        target = Waypoints.wayPoints[wavePointIndex];
+    }
+
+    private void Update()
+    {
+        Vector3 dir = target.position - transform.position;
+        transform.Translate(dir.normalized * speed * Time.deltaTime);
+
+        if (Vector3.Distance(transform.position, target.position) <= 0.2f)
+        {
+            getNextWayPoint();
+        }
+    }
+
+    private void getNextWayPoint()
+    {
+
+        if (++wavePointIndex == Waypoints.wayPoints.Length)
+        {
+            Destroy(gameObject);
+            return;
+        }
+        target = Waypoints.wayPoints[wavePointIndex];
+    }
+}
---
+++ FigureDefence/Assets/WaveSpawner.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WaveSpawner : MonoBehaviour
+{
+    public Transform enemyPrefab;
+    public Transform spawnPoint;
+
+    public float timeBetweenWaves = 5.0f;
+    private float countDown = 2f;
+
+    private int waveIndex = 0;
+    private void Update()
+    {
+        if(countDown <= 0.0f)
+        {
+            StartCoroutine(spawnWave());
+            countDown = timeBetweenWaves;
+        }
+
+        countDown -= Time.deltaTime;
+    }
+
+    IEnumerator spawnWave()
+    {
+        for(int i = 0; i < waveIndex; i++)
+        {
+            spawnEnemy();
+            yield return new WaitForSeconds(0.5f);
+        }
+        waveIndex++;
+    }
+
+    void spawnEnemy()
+    {
+        Instantiate(enemyPrefab, spawnPoint.position, spawnPoint.rotation);
+    }
+}
